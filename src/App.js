169bc55bbbab6471844bc73934d65cd0217b71d0
Stefanie Asaytono <stefanie@dcg.ph>
2024-01-03 12:11:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './page/Home';
 function App() {
   return (
     <div className="App">
+      {/* <Home /> */}
       <Router>
         <Routes>
           <Route path="/Home" element={<Home />}/>
